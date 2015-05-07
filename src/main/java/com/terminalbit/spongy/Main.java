c62f58817527e4ee85063d98b924adc94cbc84f7
SnakeByte <lokio.casebstv@gmail.com>
2015-05-07 18:55:25 -0500

    :)

--- src/main/java/com/terminalbit/spongy/Main.java
+++ src/main/java/com/terminalbit/spongy/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
 
 import org.slf4j.Logger;
@@ -19,9 +20,11 @@ import org.spongepowered.api.event.state.PreInitializationEvent;
 import org.spongepowered.api.event.state.ServerStartedEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.service.command.CommandService;
+import org.spongepowered.api.service.config.ConfigDir;
 import org.spongepowered.api.service.config.DefaultConfig;
 import org.spongepowered.api.text.Texts;
 import org.spongepowered.api.util.command.spec.CommandSpec;
+
 import static org.spongepowered.api.util.command.args.GenericArguments.*;
 
 import com.google.inject.Inject;
@@ -35,87 +38,101 @@ import com.terminalbit.spongy.command.setWarp;
 import com.terminalbit.spongy.command.simpleTP;
 import com.terminalbit.spongy.command.simpleTPHERE;
 
-@Plugin(id = "Spongy", name="Spongy", version="0.3")
-public class Main {	
+@Plugin(id = "Spongy", name = "Spongy", version = "0.3")
+public class Main {
 	@Inject
 	public Logger logger;
-	
+
 	@Inject
 	public Game game;
-	
+
 	public static Main access;
-	
+
 	@Inject
 	@DefaultConfig(sharedRoot = false)
 	public ConfigurationLoader<CommentedConfigurationNode> configManager;
-
+	
 	@Inject
 	@DefaultConfig(sharedRoot = false)
-	private File defaultConfig;
+	private File configDir;
 	
+	@ConfigDir(sharedRoot = false)
+	private File userConfigFile = new File(this.configDir, "users.conf");
+
+	public ConfigurationLoader<CommentedConfigurationNode> userConfigManager = HoconConfigurationLoader.builder().setFile(userConfigFile).build();
+	public ConfigurationNode userConfig = null;
+
+
 	private boolean useJoinSound;
 	public ConfigurationNode config = null;
+
 	@Subscribe
-	public void PreInitialization(PreInitializationEvent event){
+	public void PreInitialization(PreInitializationEvent event) {
 		Plugin plugin = Main.class.getAnnotation(Plugin.class);
 		logger.info("Spongy v" + plugin.version() + " is starting...");
 		try {
-		     if (!defaultConfig.exists()) {
-		    	defaultConfig.getParentFile().mkdirs();
-		        defaultConfig.createNewFile();
-		        config = configManager.load();
-		        config.getNode("version").setValue(plugin.version());
+			userConfig = userConfigManager.load();
+			userConfigManager.save(userConfig);
+			if (!configDir.exists()) {
+				configDir.getParentFile().mkdirs();
+				configDir.createNewFile();
+				config = configManager.load();
+				config.getNode("version").setValue(plugin.version());
 				config.getNode("playJoinSound").setValue(false);
-		        configManager.save(config);
-		    }
-		    config = configManager.load();
-		    config.getNode("version").setValue(plugin.version());
-		    useJoinSound = config.getNode("playJoinSound").getBoolean();
-		    configManager.save(config);
+				configManager.save(config);
+			}
+			config = configManager.load();
+			config.getNode("version").setValue(plugin.version());
+			useJoinSound = config.getNode("playJoinSound").getBoolean();
+			configManager.save(config);
 
 		} catch (IOException exception) {
-		    exception.printStackTrace();
+			exception.printStackTrace();
 		}
 		access = this;
 	}
-	
+
 	@Subscribe
-	public void Initalization(InitializationEvent event){
+	public void Initalization(InitializationEvent event) {
 		CommandService cmdService = game.getCommandDispatcher();
-		CommandSpec broadcastSpec = CommandSpec.builder()
+		CommandSpec broadcastSpec = CommandSpec
+				.builder()
 				.setDescription(Texts.of("Broadcast Desc"))
-				//.setPermission("spongy.chat.broadcast") <-- Let's not... :)
+				// .setPermission("spongy.chat.broadcast") <-- Let's not... :)
 				.setArguments(optional(remainingJoinedStrings(Texts.of("all"))))
-				.setExecutor(new Broadcast())
-				.build();
+				.setExecutor(new Broadcast()).build();
 		cmdService.register(this, broadcastSpec, "broadcast");
 		cmdService.register(this, new actAsConsole(logger, game), "asconsole");
 		cmdService.register(this, new simpleTP(logger, game), "tp");
-		cmdService.register(this, new simpleTPHERE(logger, game), "tphere");	
-		cmdService.register(this, new setWarp(logger, game,configManager), "setwarp");	
-		cmdService.register(this, new Warp(logger, game,configManager), "warp");	
-		cmdService.register(this, new reloadConfig(logger, game,configManager), "reloadSpongyConfig");
-		cmdService.register(this, new Spawn(logger, game,configManager), "spawn");
-		cmdService.register(this, new setSpawn(logger, game,configManager), "setspawn");
+		cmdService.register(this, new simpleTPHERE(logger, game), "tphere");
+		cmdService.register(this, new setWarp(logger, game, configManager),
+				"setwarp");
+		cmdService
+				.register(this, new Warp(logger, game, configManager), "warp");
+		cmdService.register(this,
+				new reloadConfig(logger, game, configManager),
+				"reloadSpongyConfig");
+		cmdService.register(this, new Spawn(logger, game, configManager),
+				"spawn");
+		cmdService.register(this, new setSpawn(logger, game, configManager),
+				"setspawn");
 	}
-	
+
 	@Subscribe
-	public void onServerStart(ServerStartedEvent event){
-		/*try {
-			config = configManager.load();
-			if(config.getNode("spawn").isVirtual()){
-				logger.info("Assigning spawn location.");
-				logger.info("Assuming default world is called \"world\" to work around an unimplemented method.")
-				World world = game.getServer().getWorld("world").get();
-				Location worldSpawn = world.getSpawnLocation();
-				config.getNode("spawn","position","X").setValue(worldSpawn.getX());
-				config.getNode("spawn","position","Y").setValue(worldSpawn.getY());
-				config.getNode("spawn","position","Z").setValue(worldSpawn.getZ());
-			}
-			configManager.save(config);
-		} catch (IOException e) {
-			logger.error("Failed to set spawn location. ;( I cri.");
-		}*/
+	public void onServerStart(ServerStartedEvent event) {
+		/*
+		 * try { config = configManager.load();
+		 * if(config.getNode("spawn").isVirtual()){
+		 * logger.info("Assigning spawn location."); logger.info(
+		 * "Assuming default world is called \"world\" to work around an unimplemented method."
+		 * ) World world = game.getServer().getWorld("world").get(); Location
+		 * worldSpawn = world.getSpawnLocation();
+		 * config.getNode("spawn","position","X").setValue(worldSpawn.getX());
+		 * config.getNode("spawn","position","Y").setValue(worldSpawn.getY());
+		 * config.getNode("spawn","position","Z").setValue(worldSpawn.getZ()); }
+		 * configManager.save(config); } catch (IOException e) {
+		 * logger.error("Failed to set spawn location. ;( I cri."); }
+		 */
 		logger.info("Spongy has started.");
 		logger.info("-------------------------------------");
 		logger.info("Spongy was created by Lokio27.");
@@ -127,25 +144,30 @@ public class Main {
 		logger.info("-------------------------------------");
 		logger.info(game.getPlatform().toString());
 	}
+
 	@Subscribe
 	public void onPlayerConnection(PlayerJoinEvent event) {
-		for(Player player : game.getServer().getOnlinePlayers()){
-			//Waiting for implementation...
-			//player.playSound(SoundTypes.ORB_PICKUP,player.getLocation().getPosition(),1);
-			if(useJoinSound){
+		for (Player player : game.getServer().getOnlinePlayers()) {
+			// Waiting for implementation...
+			// player.playSound(SoundTypes.ORB_PICKUP,player.getLocation().getPosition(),1);
+			if (useJoinSound) {
 				double x = player.getLocation().getX();
 				double y = player.getLocation().getY();
 				double z = player.getLocation().getZ();
-				game.getCommandDispatcher().process(game.getServer().getConsole(), "playsound random.orb " + player.getName() + " " + x + " " + y + " " + z + " 0.5");
+				game.getCommandDispatcher().process(
+						game.getServer().getConsole(),
+						"playsound random.orb " + player.getName() + " " + x
+								+ " " + y + " " + z + " 0.5");
 			}
 		}
 	}
+
 	@SuppressWarnings("deprecation")
 	@Subscribe
 	public void onMessage(MessageEvent event) {
-		//Replace all colorcodes with actual ones
+		// Replace all colorcodes with actual ones
 		String original = Texts.toPlain(event.getMessage());
-		//TODO: Replace with a better method of parsing.
-		event.setMessage(Texts.of(Texts.replaceCodes(original,'&')));
+		// TODO: Replace with a better method of parsing.
+		event.setMessage(Texts.of(Texts.replaceCodes(original, '&')));
 	}
 }

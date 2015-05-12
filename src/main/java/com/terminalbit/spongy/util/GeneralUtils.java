653fe78fd90d5001a1897b8ffee5f1dac62955c7
Lokio27 <lokio.casebstv@gmail.com>
2015-05-12 16:40:30 -0500

    :D:D:D:D::D

--- src/main/java/com/terminalbit/spongy/util/GeneralUtils.java
+++ src/main/java/com/terminalbit/spongy/util/GeneralUtils.java
@@ -1,13 +1,23 @@
 package com.terminalbit.spongy.util;
 
+import org.spongepowered.api.Game;
 import org.spongepowered.api.entity.player.Player;
 import org.spongepowered.api.world.Location;
+import org.spongepowered.api.world.TeleportHelper;
 
 import com.flowpowered.math.vector.Vector3d;
+import com.terminalbit.spongy.Main;
 
 public class GeneralUtils {
 	public static void TeleAndRotate(Player target, Location destination, Vector3d rotation){
 		target.setRotation(rotation);
+		Game game = Main.access.game;
+		TeleportHelper myTeleHelper = game.getTeleportHelper();
+		if(myTeleHelper.getSafeLocation(destination).isPresent()){
+			target.setLocation(myTeleHelper.getSafeLocation(destination).get());
+		}else{
+			target.setLocation(destination);
+		}
 		target.setLocation(destination);
 	}
 }
